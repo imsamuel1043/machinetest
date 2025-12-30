a0ebcdaeea6a84cd7356721cfcfa0b96ea14fea8
imsamuel1043 <samuelmorris1043@gmail.com>
2025-12-30 18:06:05 +0530

    fifth commit

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
